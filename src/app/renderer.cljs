bd53a4e1e8768ab474e33301232c37f5f45911b1
iovxw <iovxw@outlook.com>
2016-09-18 18:00:12 +0800

    tab component

--- src/app/renderer.cljs
+++ src/app/renderer.cljs
@@ -24,18 +24,38 @@
     [:button.mini-size {:on-click #(.minimize current-window)}]
     [:button.close {:on-click #(.close current-window)}]]])
 
-(defn tabs []
-  [:div#tabs>div
-   ])
+(defn tabs [tabs-state title-list]
+  (into
+   [:div#tabs>div]
+   (for [title title-list]
+     (let [id (hash title)]
+       [:div.tab
+        {:class (when (= id (:focus @tabs-state)) "focus" )
+         :on-click (fn [e] (swap! tabs-state assoc :focus id))}
+        [:a title]]))))
 
-(defn tab-contents []
-  [:div#tab-contents>div
-   ])
+(defn tab-contents [tabs-state contents]
+  (into
+   [:div#tab-contents>div]
+   (for [[title content] (partition 2 contents)]
+     (let [id (hash title)]
+       [:div.block
+        {:style (when-not (= id (:focus @tabs-state))
+                  {:display "none"})}
+        [:h1 title]
+        content]))))
+
+(defn contents [& contents]
+  (let [title-list (take-nth 2 contents)
+        tabs-state (r/atom {:focus (hash (first title-list))})]
+    [:div#contents
+     [tabs tabs-state title-list] [tab-contents tabs-state contents]]))
 
 (defn body []
   [:div [title-bar]
-   [:div#contents
-    [tabs] [tab-contents]]])
+   [contents
+    "全局设置" [:div "..."]
+    "自动切换" [:div "..."]]])
 
 (defn init []
   (enable-console-print!)
