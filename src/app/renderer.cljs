ee544935645ba2d48f55de73d9144770953567e5
iovxw <iovxw@outlook.com>
2016-09-24 00:12:26 +0800

    add config file

--- src/app/renderer.cljs
+++ src/app/renderer.cljs
@@ -74,23 +74,80 @@
 (defn get-user-home []
   (or js/process.env.HOME js/process.env.HOMEPATH js/process.env.USERPROFILE))
 
+;; 注意，此处最大屏幕不是物理上的，而是最大屏幕长和最大屏幕宽
+(defn get-largest-screen-size []
+  (let [all-displays (.getAllDisplays (.-screen electron))
+        width (max-key #(.-width %) all-displays)
+        height (max-key #(.-height %) all-displays)]
+    {:width width :height height}))
+
 (def state (r/atom {:working? false
                     :wallpapers []}))
 
-(def config (r/atom {:save-path (str (get-user-home) "/.cache/bismuth/")
-                     :max-wp-num 100
-                     :width 1920
-                     :height 1080}))
+(def config (let [{:keys [width height]} (get-largest-screen-size)]
+              (r/atom {:save-path (str (get-user-home) "/.cache/bismuth/")
+                       :max-wp-num 100
+                       :width width
+                       :height height})))
+
+(def config-dir (str (get-user-home) "/.config/bismuth/"))
+(def config-file (str config-dir "config.json"))
 
 (defn file-exist? [path]
   (let [result-chan (chan)]
-    (.stat fs (:save-path @config)
+    (.stat fs path
            #(if %
               (if (= (.-code %) "ENOENT")
                 (put! result-chan {:result false})
                 (put! result-chan {:error %}))
               (put! result-chan {:result true})))
     result-chan))
+
+(defn write-file [file data & {:keys [encoding mode flag]
+                               :or {encoding "utf8" mode 0666 flag "w"}}]
+  (let [error-chan (chan)]
+    (.writeFile fs file data encoding mode flag
+                #(if % (put! error-chan {:error %}) (close! error-chan)))
+    error-chan))
+
+(defn read-file [file & {:keys [encoding flag]
+                         :or {encoding nil flag "r"}}]
+  (let [result-chan (chan)]
+    (.readFile fs file encoding flag
+               #(if %1
+                  (put! result-chan {:error %1})
+                  (put! result-chan {:result %2})))
+    result-chan))
+
+(defn do-for-every-key [m func]
+  (if (and (map? m) func)
+    (into
+     {}
+     (for [[k v] m]
+       [(func k) (do-for-every-key v func)]))
+    m))
+
+(defn read-json-str [string & {:keys [key-fn value-fn]
+                               :or {key-fn nil value-fn nil}}]
+  (do-for-every-key (js->clj (js/JSON.parse string nil)) key-fn))
+
+(defn write-json-str [data & {:keys [space key-fn value-fn]
+                              :or {space nil key-fn nil value-fn nil}}]
+  (js/JSON.stringify (clj->js (do-for-every-key data key-fn)) value-fn space))
+
+(defn save-config []
+  (write-file config-file
+              (write-json-str @config :space 4)))
+
+(defn mkdir
+  ([path] (mkdir path 0777))
+  ([path mode]
+   (let [error-chan (chan)]
+     (.mkdir fs path mode
+             #(if (and % (not= (.-code %) "EEXIST"))
+                (put! error-chan {:error %})
+                (close! error-chan)))
+     error-chan)))
 
 (defn save-wallpaper [wallpaper-base64]
   (let [data (subs wallpaper-base64 22)
@@ -99,11 +156,9 @@
         result-chan (chan)]
     (go-try
      (when-not (<!-with-err (file-exist? (:save-path @config)))
-       (.mkdir fs (:save-path @config) #(when % (put! result-chan {:error %}))))
-     (.writeFile fs file-path data "base64"
-                 #(if %
-                    (put! result-chan {:error %})
-                    (put! result-chan {:result file-path})))
+       (<!-with-err (mkdir (:save-path @config))))
+     (<!-with-err (write-file file-path data :encoding "base64"))
+     (put! result-chan {:result file-path})
      (catch :default e
        (put! result-chan {:error e})))
     result-chan))
@@ -128,13 +183,32 @@
       :disabled (:working? @state)}
      [:div "新壁纸"]]]])
 
+(defn generator-setting []
+  [:div
+   [:p "生成器选择#未完工"]
+   [:label "line" [:input {:type "checkbox"}]]
+   [:p "生成壁纸大小"]
+   [:p
+    "宽:" [:input {:value (:width @config) :type "number"
+                   :on-change #(do (swap! config assoc :width (-> % .-target .-value))
+                                   (save-config))}]
+    "高:" [:input {:value (:height @config) :type "number"
+                   :on-change #(do (swap! config assoc :height (-> % .-target .-value))
+                                   (save-config))}]]
+   [:p "壁纸保存路径"
+    [:input {:value (:save-path @config)
+             :on-change #(do (swap! config assoc :save-path (-> % .-target .-value))
+                             (save-config))}]]
+   [:p "最大壁纸保留数量#未完工"
+    [:input {:value (:max-wp-num @config) :type "number"
+             :on-change #(do (swap! config assoc :max-wp-num (-> % .-target .-value))
+                             (save-config))}]]])
+
 (defn body []
   [:div [title-bar]
    [contents
     "壁纸设置" [preview]
-    "生成器设置" [:div
-                  [:p "生成器选择"]
-                  [:label "line" [:input {:type "checkbox"}]]]
+    "生成器设置" [generator-setting]
     "自动刷新" [:div
                 [:label "启用" [:input {:type "checkbox"}]]
                 [:p "时间间隔" [:input]]
@@ -147,4 +221,15 @@
 
 (defn init []
   (enable-console-print!)
-  (r/render body (query-selector "#app")))
+  (go-try
+   (if (<!-with-err (file-exist? config-file))
+     (let [data (<!-with-err (read-file config-file))
+           cfg (read-json-str data :key-fn keyword)]
+       (reset! config cfg))
+     (do (<!-with-err (mkdir config-dir))
+         (<!-with-err
+          (write-file config-file
+                      (write-json-str @config :space 4)))))
+   (r/render body (query-selector "#app"))
+   (catch :default e
+     (js/console.error e))))
