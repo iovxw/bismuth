11aac6cc7feb27e3c1bea5e39b265d5c61c6c663
iovxw <iovxw@outlook.com>
2016-09-27 19:40:29 +0800

    fix json :key-fn for sequential

--- src/app/json.cljs
+++ src/app/json.cljs
@@ -1,16 +1,21 @@
 (ns app.json)
 
 (defn- do-for-every-key [m func]
-  (if (and (map? m) func)
-    (into
-     {}
-     (for [[k v] m]
-       [(func k) (do-for-every-key v func)]))
+  (if func
+    (cond
+      (map? m) (into
+                {}
+                (for [[k v] m]
+                  [(func k) (do-for-every-key v func)]))
+
+      (sequential? m) (vec (map #(do-for-every-key % func) m))
+
+      :default m)
     m))
 
 (defn read-str [string & {:keys [key-fn value-fn]
                           :or {key-fn nil value-fn nil}}]
-  (do-for-every-key (js->clj (js/JSON.parse string nil)) key-fn))
+  (do-for-every-key (js->clj (js/JSON.parse string value-fn)) key-fn))
 
 (defn write-str [data & {:keys [space key-fn value-fn]
                          :or {space nil key-fn nil value-fn nil}}]
